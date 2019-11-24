a93d559755ed174a9765634ccf7ffc040738d82f
Joao da Silva <joao.lennon@totvs.com.br>
2019-11-24 14:59:14 -0300

    Interface de repositório movido pra stack de escrita.

--- backend/src/Write/Repositories/Interfaces.cs
+++ backend/src/Write/Repositories/Interfaces.cs
@@ -1,8 +1,19 @@
-﻿using CrossCutting;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Write.Pocos;
 
 namespace Write.Repositories
 {
+    public interface IRepository<TEntity>
+    {
+        TEntity Add(TEntity entity);
+        Task<TEntity> Find(Guid key);
+        IQueryable<TEntity> GetAll();
+        Task<TEntity> Delete(Guid key);
+        TEntity Delete(TEntity entity);
+        Task Commit();
+    }
     public interface IBarbecueRepository : IRepository<Barbecue>
     {
     }
--- backend/src/Write/Repository.cs
+++ backend/src/Write/Repository.cs
@@ -1,7 +1,7 @@
-﻿using CrossCutting;
-using System;
+﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Write.Repositories;
 
 namespace Write
 {
